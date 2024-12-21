2e4cc333c8cf2b6bb1fad52b8f1dd3f2d2cacbff
Juan Flores <floresjuan1091@gmail.com>
2024-12-20 21:03:30 -0300

    Se agrega contenido dinámico al hacer click sobre las imágenes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const menu = document.getElementById('menu');
 const indicador = document.getElementById('indicador');
 const secciones = document.querySelectorAll('.seccion');
-const imagen = document.getElementById('image');
+const inicio = document.getElementById('inicio');
+const contenido = document.getElementById('contenido');
+const volver = document.getElementById('volver');
+const seccionDinamica = document.getElementById('contenedor-secciones-dinamicas');
 
 let tamañoIndicador = menu.querySelector('a').offsetWidth;
 indicador.style.width = tamañoIndicador + 'px';
@@ -42,5 +45,64 @@ const onResize = () => {
 	// Volvemos a posicionar el indicador.
 	indicador.style.transform = `translateX(${tamañoIndicador * indexSeccionActiva}px)`;
 }
+
+// Configuración de datos de las secciones
+const datos = [
+    {
+        id: 1,
+        titulo: "Egipto",
+        imagen: "img/num_egypt.png",
+        texto: "Los sistemas de numeración del antiguo Egipto son un fascinante ejemplo..."
+    },
+	{
+        id: 2,
+        titulo: "La India",
+        imagen: "img/num_indian.png",
+        texto: "El sistema de numeración indio fue el precursor del sistema decimal moderno..."
+    },
+    {
+        id: 3,
+        titulo: "Babilonia",
+        imagen: "img/num_babylonian.png",
+        texto: "El sistema de numeración babilónico era sexagesimal, es decir, basado en el número 60..."
+    },
+    {
+        id: 4,
+        titulo: "Los Mayas",
+        imagen: "img/num_maya.png",
+        texto: "El sistema de numeración maya era vigesimal, basado en el número 20..."
+    },
+    {
+        id: 5,
+        titulo: "Roma",
+        imagen: "img/num_roman.png",
+        texto: "El sistema de numeración romano se originó en la antigua Roma..."
+    },
+];
+
+// Crear eventos en las imágenes
+document.querySelectorAll('.seccion img').forEach((img, index) => {
+    img.addEventListener('click', () => {
+        mostrarContenido(datos[index]);
+    });
+});
+
+// Función para mostrar contenido dinámico
+function mostrarContenido(data) {
+    inicio.style.display = 'none'; // Oculta la sección inicial
+    contenido.style.display = 'block'; // Muestra la sección dinámica
+	seccionDinamica.style.display = 'flex'; //Muestra los datos
+    seccionDinamica.innerHTML = `
+        <h1>${data.titulo}</h1>
+        <img src="${data.imagen}" alt="${data.titulo}">
+        <p>${data.texto}</p>`
+};
+
+// Evento para el botón "Volver"
+volver.addEventListener('click', () => {
+    contenido.style.display = 'none'; // Oculta la sección dinámica
+    inicio.style.display = 'block'; // Muestra la sección inicial
+	seccionDinamica.style.display = 'none'; //Oculta los datos
+});
 
 window.addEventListener('resize', onResize);
